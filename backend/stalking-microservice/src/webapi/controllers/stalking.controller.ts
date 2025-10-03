a312821bab55623523e3361f43f4af2b50ae0fad
unewMe <chudzickid@gmail.com>
2025-10-03 17:29:22 +0200

    fix(stalking-microservice): rebase stalking-ms with main

--- backend/stalking-microservice/src/webapi/controllers/stalking.controller.ts
+++ backend/stalking-microservice/src/webapi/controllers/stalking.controller.ts
@@ -1,10 +1,4 @@
 // src/webapi/controllers/order.controller.ts
-<<<<<<< HEAD
-import { Controller } from "@nestjs/common";
-import { CommandBus, QueryBus } from "@nestjs/cqrs";
-
-@Controller("stalking")
-=======
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { ApiTags, ApiExtraModels } from '@nestjs/swagger';
@@ -13,7 +7,6 @@ import { StalkingAnalyzeRequestDto } from 'src/domain/models/stalking-analyze-re
 @ApiExtraModels(StalkingAnalyzeRequestDto)
 @ApiTags('stalking')
 @Controller('stalking')
->>>>>>> b8c32b6 (docs(backend): add swager + openapi)
 export class StalkingController {
   constructor(
     private readonly commandBus: CommandBus,

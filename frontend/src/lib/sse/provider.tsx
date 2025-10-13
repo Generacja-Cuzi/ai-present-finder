f67e038bfc88b70b5e49a030474f0ad5da193f33
Dawid Chudzicki <56974648+unewMe@users.noreply.github.com>
2025-10-13 15:18:20 +0200

    refactor(events): move SSE to shared abstraction (#72)
    * refactor(events): move SSE to shared abstraction
    
    * fix: link
    
    * refactor(gift-searching): change props to inline, rename folders,files

--- frontend/src/lib/sse/provider.tsx
+++ frontend/src/lib/sse/provider.tsx
@@ -3,7 +3,7 @@ import { SSEProvider } from "react-hooks-sse";
 
 import { getBackendUrl } from "@/lib/backend-url";
 
-export function SseProviderWrapper({
+export function CustomSseProvider({
   children,
   clientId,
 }: {
@@ -12,5 +12,6 @@ export function SseProviderWrapper({
 }) {
   const baseUrl = getBackendUrl();
   const sseEndpoint = `${baseUrl}/sse?clientId=${clientId}`;
+
   return <SSEProvider endpoint={sseEndpoint}>{children}</SSEProvider>;
 }

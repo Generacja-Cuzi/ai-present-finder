641edd0ecba97c6ff310a3069d0bad36b0d6b9f4
Dawid Chudzicki <56974648+unewMe@users.noreply.github.com>
2025-10-13 16:41:51 +0200

    feat: add home view (#76)

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -4,6 +4,8 @@ import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { Toaster } from "sonner";
 
+import { Navbar } from "@/components/ui/navbar";
+
 import { tanstackQueryDevtools } from "../lib/tanstack-query/devtools";
 
 interface MyRouterContext {
@@ -14,6 +16,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Outlet />
+      <Navbar />
       <Toaster />
       <TanStackDevtools
         config={{

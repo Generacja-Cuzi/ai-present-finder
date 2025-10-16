b86851191e1d193c9268f72c59ae8c278ae0a147
Szymon Kowaliński <szymon@kowalinski.dev>
2025-10-16 18:55:53 +0200

    feat(chat): include occasion in the chat context (#95)

--- backend/chat-microservice/src/domain/commands/set-occasion.command.ts
+++ backend/chat-microservice/src/domain/commands/set-occasion.command.ts
@@ -1,12 +1,9 @@
-import type { ChatMessage } from "@core/types";
-
 import { Command } from "@nestjs/cqrs";
 
-export class GenerateQuestionCommand extends Command<void> {
+export class SetOccasionCommand extends Command<void> {
   constructor(
     public readonly chatId: string,
     public readonly occasion: string,
-    public readonly history: ChatMessage[],
   ) {
     super();
   }

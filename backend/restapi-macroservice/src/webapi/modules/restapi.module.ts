7e3222e51ebce3bfbe32260075749c8caa1d848b
unewMe <chudzickid@gmail.com>
2025-10-19 15:07:07 +0200

    ci: turn on restapi database

--- backend/restapi-macroservice/src/webapi/modules/restapi.module.ts
+++ backend/restapi-macroservice/src/webapi/modules/restapi.module.ts
@@ -36,8 +36,6 @@ import { ChatController } from "../controllers/chat.controller";
 import { RestApiController } from "../controllers/restapi.controller";
 import { SseController } from "../controllers/sse.controller";
 
-const isCI = process.env.NODE_ENV === "ci";
-
 @Module({
   imports: [
     CqrsModule,
@@ -54,33 +52,26 @@ const isCI = process.env.NODE_ENV === "ci";
       }),
       inject: [ConfigService],
     }),
-    ...(isCI
-      ? []
-      : [
-          TypeOrmModule.forRootAsync({
-            useFactory: (configService: ConfigService) => ({
-              type: "postgres" as const,
-              host: configService.get<string>("DATABASE_HOST") ?? "localhost",
-              port: Number.parseInt(
-                configService.get<string>("DATABASE_PORT") ?? "5433",
-                10,
-              ),
-              username:
-                configService.get<string>("DATABASE_USERNAME") ??
-                "restapi_user",
-              password:
-                configService.get<string>("DATABASE_PASSWORD") ??
-                "restapi_password",
-              database:
-                configService.get<string>("DATABASE_NAME") ?? "restapi_db",
-              entities: [User, Chat],
-              synchronize: true,
-              logging: false,
-            }),
-            inject: [ConfigService],
-          }),
-          TypeOrmModule.forFeature([User, Chat]),
-        ]),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres" as const,
+        host: configService.get<string>("DATABASE_HOST") ?? "localhost",
+        port: Number.parseInt(
+          configService.get<string>("DATABASE_PORT") ?? "5433",
+          10,
+        ),
+        username:
+          configService.get<string>("DATABASE_USERNAME") ?? "restapi_user",
+        password:
+          configService.get<string>("DATABASE_PASSWORD") ?? "restapi_password",
+        database: configService.get<string>("DATABASE_NAME") ?? "restapi_db",
+        entities: [User, Chat],
+        synchronize: true, // Only for development
+        logging: false,
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([User, Chat]),
     ClientsModule.register([
       {
         name: "STALKING_ANALYZE_REQUESTED_EVENT",

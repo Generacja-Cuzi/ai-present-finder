9e0c236244f238227f9d4aa4953de0d85cce99ef
unewMe <chudzickid@gmail.com>
2025-10-19 17:11:42 +0200

    feat: add types to auth commands and queries

--- backend/restapi-macroservice/src/domain/commands/validate-google-token.command.ts
+++ backend/restapi-macroservice/src/domain/commands/validate-google-token.command.ts
@@ -1,3 +1,12 @@
-export class ValidateGoogleTokenCommand {
-  constructor(public readonly code: string) {}
+import { Command } from "@nestjs/cqrs";
+
+import { User } from "../entities/user.entity";
+
+export class ValidateGoogleTokenCommand extends Command<{
+  accessToken: string;
+  user: User;
+}> {
+  constructor(public readonly code: string) {
+    super();
+  }
 }
--- backend/restapi-macroservice/src/domain/queries/get-user-chats.query.ts
+++ backend/restapi-macroservice/src/domain/queries/get-user-chats.query.ts
@@ -1,3 +1,9 @@
-export class GetUserChatsQuery {
-  constructor(public readonly userId: string) {}
+import { Query } from "@nestjs/cqrs";
+
+import { Chat } from "../entities/chat.entity";
+
+export class GetUserChatsQuery extends Query<Chat[]> {
+  constructor(public readonly userId: string) {
+    super();
+  }
 }

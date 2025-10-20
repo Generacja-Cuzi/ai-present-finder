dcb14711e6de6e90ed2b70e6f2590acbf6b5a4df
Marcin Dolatowski <marcindolatowski123@gmail.com>
2025-10-20 22:31:08 +0200

    fix: types

--- backend/reranking-microservice/src/domain/commands/add-products-to-session.command.ts
+++ backend/reranking-microservice/src/domain/commands/add-products-to-session.command.ts
@@ -1,12 +1,12 @@
 import type { ProductFetchedEvent } from "@core/events";
-import type { ListingDto } from "@core/types";
+import type { ListingPayload } from "@core/types";
 
 type FetchProvider = ProductFetchedEvent["provider"];
 
 export class AddProductsToSessionCommand {
   constructor(
     public readonly eventId: string,
-    public readonly products: ListingDto[],
+    public readonly products: ListingPayload[],
     public readonly sourceEventName: string,
     public readonly sourceEventProvider: FetchProvider,
     public readonly sourceEventSuccess: boolean,

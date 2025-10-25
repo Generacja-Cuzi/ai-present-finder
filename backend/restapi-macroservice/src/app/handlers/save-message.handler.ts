448310b598bbc43fe4b4e49e185e41ed2db1c800
Szymon Kowaliński <szymon@kowalinski.dev>
2025-10-25 13:50:37 +0200

    feat(chat): save chat messages history

---
+++ backend/restapi-macroservice/src/app/handlers/save-message.handler.ts
@@ -0,0 +1,21 @@
+import { SaveMessageCommand } from "src/domain/commands/save-message.command";
+import { IChatRepository } from "src/domain/repositories/ichat.repository";
+import { IMessageRepository } from "src/domain/repositories/imessage.repository";
+
+import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+
+@CommandHandler(SaveMessageCommand)
+export class SaveMessageHandler implements ICommandHandler<SaveMessageCommand> {
+  constructor(
+    private readonly messageRepository: IMessageRepository,
+    private readonly chatRepository: IChatRepository,
+  ) {}
+
+  async execute(command: SaveMessageCommand): Promise<void> {
+    await this.messageRepository.create({
+      chatId: command.chatId,
+      content: command.content,
+      role: command.role,
+    });
+  }
+}

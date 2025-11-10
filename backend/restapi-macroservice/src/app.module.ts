2c0f13f4f7f4a306a09c43bd341d08235c973d53
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-11-10 13:46:52 +0100

    refactor: remove unused AppController and its tests

--- backend/restapi-macroservice/src/app.module.ts
+++ backend/restapi-macroservice/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 
-import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { GoogleService } from "./app/services/google-service";
 import { HealthModule } from "./webapi/modules/health.module";
@@ -15,7 +14,6 @@ import { RestApiModule } from "./webapi/modules/restapi.module";
     RestApiModule,
     HealthModule,
   ],
-  controllers: [AppController],
   providers: [AppService, GoogleService, ConfigService],
 })
 export class AppModule {}
